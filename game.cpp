8c7c34077b5290120610c184b2d047f53de3509a
lborgesc <lborgescaetano@gmail.com>
2023-09-13 17:35:04 -0300

    inserido logica de random

--- game.cpp
+++ game.cpp
@@ -2,71 +2,78 @@
 #include <fstream>
 #include <locale>
 #include <vector>
+#include <ctime>  
 #include <random>
 
 using namespace std; 
 
 vector<string> arrayModalidade( string txt );
-int numRandom(int min, int max);
+int numRandom( int min, int max );
 
-int main() {
+int main( ) {
 
-    string escolha; 
-    vector<string> palavras;
-    int min = 0;
-    int max;
+    
+    string escolha, forca; 
+    vector< string > palavras;
+    int min = 0, max, loop;
 
     cout << "Escolha a modalidade que dejesa jogar: Futebol/Filmes/Carros" << endl; 
     cin >> escolha; 
 
     for ( char& c : escolha ) {
-        c = tolower(c);
+        c = tolower( c );
     }
 
     escolha = escolha + ".txt";
   
     palavras = arrayModalidade( escolha );
-    max = palavras.size( );
+    max = ( sizeof( palavras ) / sizeof( int ) );
 
-    int random = numRandom( min, max );
+    srand( time( NULL ) );
+    int random = ( min + rand( ) % max );
 
-    cout << random;
+    forca = palavras[ random ];
+
+    loop = round( forca.length( ) / 2 );
+
+    int arrayForca[ loop ];
+    
+    for ( int i = 0; i < loop; i++ ){
+        srand( time( NULL ) * i );
+        cout << forca << endl; 
+        cout << forca[ min + rand( ) % random ] << endl;        
+        arrayForca[ i ] = forca[ min + rand( ) % random ];
+
+    }
+
+    cout << "array: " << arrayForca[ 0 ];
 
     return 0;
 }
 
 
 
- vector<string> arrayModalidade( string txt ){
+
+
+ vector< string > arrayModalidade( string txt ){
 
     ifstream arquivo( txt );
     string linha;
-    vector<string> palavras;
+    vector< string > palavras;
     
 
-    if (!arquivo.is_open()) {
+    if ( !arquivo.is_open( ) ) {
         cerr << "Erro ao abrir o arquivo." << endl;
     }
 
     while ( getline( arquivo, linha ) ) {
-        palavras.push_back(linha);
+        palavras.push_back( linha );
     }
        
-    arquivo.close();
+    arquivo.close( );
 
     return palavras;
  }
 
- int numRandom(int min, int max) {
-    
-    random_device rd;
-    
-    
-    mt19937 gen(rd());
-    
-    
-    uniform_int_distribution<int> dis(min, max);
-    
-    
-    return dis(gen);
-}
+
+
